f31aa5845a36957c8f4ba22ef10151f56389cff4
Michael Walker <mike@barrucadu.co.uk>
2014-05-11 04:25:22 +0100

    Remove excess imports

--- src/Types.hs
+++ src/Types.hs
@@ -2,13 +2,11 @@
 
 module Types where
 
-import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Reader (ReaderT)
 import Database.Persist.Sql (SqlPersistM)
 import Data.Text (Text)
 import Network.Wai (Request, Response)
 import Routes (Sitemap)
-import Text.Hamlet (HtmlUrl)
 
 -- |Function to make URLs from a Sitemap
 type MkUrl = Sitemap -> [(Text, Maybe Text)] -> Text
