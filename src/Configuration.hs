6c7f71f008c585cc43513bc0444a992e619ea3fa
Michael Walker <mike@barrucadu.co.uk>
2014-05-13 00:10:49 +0100

    Tidy Configuration

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -4,6 +4,7 @@ module Configuration (ConfigParser,
                       loadConfigFile, defaults,
                       get, get', conf, conf') where
 
+import Control.Applicative ((<$>))
 import Control.Monad.Error.Class (MonadError)
 import Data.ConfigFile
 import Data.Either.Utils (forceEither)
@@ -24,32 +25,36 @@ import Types (RequestProcessor, askConf)
 -- application to their defaults if they weren't present already, but
 -- other values are left as they are in the file.
 loadConfigFile :: FilePath -> IO (Maybe ConfigParser)
-loadConfigFile filename = loadCP `catchIOError` const (return Nothing)
+loadConfigFile filename = (Just <$> loadConfigFileUnsafe filename) `catchIOError` const (return Nothing)
 
-    where loadCP = do let base = emptyCP { accessfunc = interpolatingAccess 10 }
-                      cp <- readfile base filename
-                      return . Just . merge defaults $ forceEither cp
+-- |Load a configuration file unsafely. This may throw an IO
+-- exception.
+loadConfigFileUnsafe :: FilePath -> IO ConfigParser
+loadConfigFileUnsafe filename = do
+  let base = emptyCP { accessfunc = interpolatingAccess 10 }
+  cp <- readfile base filename
+  return . merge defaults $ forceEither cp
 
 -- |Default configuration values
 defaults :: ConfigParser
 defaults = forceEither . readstring emptyCP $ unlines
-           [ "[server]"
-           , "host      = *"
-           , "port      = 3000"
-           , "web_root  = http://localhost:3000"
-           , "file_root = /tmp"
-           , "[database]"
-           , "connection_string = lambdadelta.sqlite"
-           , "pool_size         = 10"
-           , "[board]"
-           , "board_listing = [[\"b\"]]"
-           , "summary_size = 5"
-           , "threads_per_page = 10"
-           , "maximum_pages = 10"
-           , "thumbnail_width = 300"
-           , "thumbnail_height = 300"
-           , "bump_limit = 300"
-           ]
+  [ "[server]"
+  , "host      = *"
+  , "port      = 3000"
+  , "web_root  = http://localhost:3000"
+  , "file_root = /tmp"
+  , "[database]"
+  , "connection_string = lambdadelta.sqlite"
+  , "pool_size         = 10"
+  , "[board]"
+  , "board_listing = [[\"b\"]]"
+  , "summary_size = 5"
+  , "threads_per_page = 10"
+  , "maximum_pages = 10"
+  , "thumbnail_width = 300"
+  , "thumbnail_height = 300"
+  , "bump_limit = 300"
+  ]
 
 -- |Get a value from the configuration unsafely
 get' :: Get_C a => ConfigParser -> SectionSpec -> OptionSpec -> a
@@ -58,10 +63,8 @@ get' cp ss os = forceEither $ get cp ss os
 -- |Get a value from the configuration in a handler, abstracting the
 -- askConf/get pattern
 conf :: (Get_C a, MonadError CPError m) => SectionSpec -> OptionSpec -> RequestProcessor (m a)
-conf ss os = do config <- askConf
-                return $ get config ss os
+conf ss os = askConf >>= \config -> return $ get config ss os
 
 -- |Get a value from the configuration in a handler unsafely
 conf' :: Get_C a => SectionSpec -> OptionSpec -> RequestProcessor a
-conf' ss os = do config <- askConf
-                 return $ get' config ss os
+conf' ss os = askConf >>= \config -> return $ get' config ss os
