f5e5c57b89535160923d4d111b2da11576617e29
Michael Walker <mike@barrucadu.co.uk>
2014-05-24 01:51:26 +0100

    Split up Web.Seacat.RequestHandler.BanHammer into helper functions

--- seacat/src/Web/Seacat/RequestHandler/BanHammer.hs
+++ seacat/src/Web/Seacat/RequestHandler/BanHammer.hs
@@ -1,20 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Web.Seacat.RequestHandler.BanHammer ( ipBan
-                                           , rateLimit) where
+                                           , rateLimit
+                                           , rateLimit') where
 
 import Control.Applicative ((<$>))
 import Control.Monad.IO.Class (liftIO)
 import Data.Bits ((.|.), shift)
 import Data.Text (Text)
 import Data.Time.Clock (NominalDiffTime, UTCTime, addUTCTime, getCurrentTime)
+import Data.Maybe (isJust)
 import Data.Word ()
 import Database.Persist
 import Network.Socket (SockAddr(..))
 import Network.Wai (remoteHost)
 import Web.Routes.PathInfo (PathInfo)
 import Web.Seacat.Database
-import Web.Seacat.RequestHandler.Types (Handler, askReq)
+import Web.Seacat.RequestHandler.Types (Handler, RequestProcessor, askReq)
 
 -- |Rate limit a particular route. This only looks at individual IPs,
 -- and not ranges.
@@ -31,6 +33,31 @@ rateLimit :: PathInfo r
           -> Handler r -- ^ The handler
           -> Handler r
 rateLimit tag freq onLimit handler = do
+  isLimited <- isRateLimited tag
+  now       <- liftIO getCurrentTime
+  ip        <- (show . remoteHost) <$> askReq
+
+  if isLimited
+  then onLimit
+  else do
+    let banUntil = addUTCTime freq now
+    insert $ RateLimit tag banUntil ip
+    handler
+
+-- |Don't let someone past if they are rate limited, but don't set a
+-- new limit if there isn't one already.
+rateLimit' :: PathInfo r => Maybe String -> Handler r -> Handler r -> Handler r
+rateLimit' tag onLimit handler = do
+  isLimited <- isRateLimited tag
+
+  if isLimited
+  then onLimit
+  else handler
+
+-- | Check if an IP is rate limited. This has the side-effect of
+-- clearing out all expired limits.
+isRateLimited :: PathInfo r => Maybe String -> RequestProcessor r Bool
+isRateLimited tag = do
   ip <- (show . remoteHost) <$> askReq
 
   now <- liftIO getCurrentTime
@@ -44,12 +71,7 @@ rateLimit tag freq onLimit handler = do
                      , RateLimitTarget  ==. ip
                      ]) []
 
-  case ban of
-    Just _  -> onLimit
-    Nothing -> do
-      let banUntil = addUTCTime freq now
-      insert $ RateLimit tag banUntil ip
-      handler
+  return $ isJust ban
 
 --------------------
 
@@ -73,25 +95,27 @@ ipBan tag onBan handler = do
                      , IPBanTarget  ==. show ip
                      ]) []
 
-  let intIp = case ip of
-                SockAddrInet _ hostaddr -> fromIntegral hostaddr
-                SockAddrInet6 _ _ (a, b, c, d) _ -> let a'  = shift (fromIntegral a :: Integer) 96
-                                                        b' = shift (fromIntegral b :: Integer) 64
-                                                        c' = shift (fromIntegral c :: Integer) 32
-                                                        d' = fromIntegral d
-                                                    in fromIntegral $ a' .|. b' .|. c' .|. d'
-                SockAddrUnix _ -> -1
-
   rangeBan <- selectFirst ([ IPRangeBanApplies ==. tag
-                          , IPRangeBanStart <=. intIp
-                          , IPRangeBanStop >=. intIp
+                          , IPRangeBanStart <=. ipToRational ip
+                          , IPRangeBanStop >=. ipToRational ip
                           ] ||.
                           [ IPRangeBanApplies ==. Nothing
-                          , IPRangeBanStart <=. intIp
-                          , IPRangeBanStop >=. intIp
+                          , IPRangeBanStart <=. ipToRational ip
+                          , IPRangeBanStop >=. ipToRational ip
                           ]) []
   case ban of
     Just (Entity _ ipban) -> onBan (iPBanExpires ipban) (iPBanReason ipban)
     Nothing -> case rangeBan of
                 Just (Entity _ rangeban) -> onBan (iPRangeBanExpires rangeban) (iPRangeBanReason rangeban)
                 Nothing -> handler
+
+-- |Convert an IP address into a Rational, the type I'm using in the
+-- database to represent them.
+ipToRational :: SockAddr -> Rational
+ipToRational (SockAddrInet _ hostAddr) = fromIntegral hostAddr
+ipToRational (SockAddrInet6 _ _ (a, b, c, d) _) = let a' = shift (fromIntegral a :: Integer) 96
+                                                      b' = shift (fromIntegral b :: Integer) 64
+                                                      c' = shift (fromIntegral c :: Integer) 32
+                                                      d' = fromIntegral d
+                                                  in fromIntegral $ a' .|. b' .|. c' .|. d'
+ipToRational (SockAddrUnix _) = -1
