0fa06d63e13b249e00708ae241d40c446744b7e6
Michael Walker <mike@barrucadu.co.uk>
2014-06-01 13:40:51 +0100

    Remove OverloadedStrings from Web.Seacat

--- seacat/src/Web/Seacat.hs
+++ seacat/src/Web/Seacat.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 -- |The entry point of the Seacat module hierarchy: this module
 -- re-exports many common functions and types from the rest of the
 -- library, and submodules of this comprise all Seacat functionality.
